82693c6b294b79fb309296942f4645491e23a919
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-06-15 11:38:00 -0400

    Change: moving to typescript

---
+++ src/recorder.ts
@@ -0,0 +1,9 @@
+import puppeteer from "https://deno.land/x/puppeteer@9.0.1/mod.ts";
+
+async function convertRecording(jsonPath: string, savePath: string) {
+    console.log("Hello, world!")
+}
+
+export {
+    convertRecording
+}
