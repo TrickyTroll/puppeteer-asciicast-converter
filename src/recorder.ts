bae1813c7e85a49ae425b536546d2adf2b009805
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-06-17 18:00:46 -0400

    In progress: using `puppeteer` for recording

--- src/recorder.ts
+++ src/recorder.ts
@@ -1,8 +1,12 @@
-// import puppeteer from "https://deno.land/x/puppeteer@9.0.1/mod.ts";
+// @ts-ignore
+import puppeteer from "https://deno.land/x/puppeteer@9.0.1/mod.ts";
 
-// async function convertRecording(jsonPath: string, savePath: string) {
-//     console.log("Hello, world!")
-// }
+async function convertRecording(jsonPath: string, savePath: string, pagePath: string) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(pagePath)
+    console.log("Hello, world!")
+}
 
 function greet() {
     console.log("Hello, world!")
