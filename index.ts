fa8827b473b8b42aaeb5f7f572cd8cde0e277fc9
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-06-18 13:48:22 -0400

    Fix: added shebang to execute as node script

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 // @ts-ignore
-import { getRecordings } from "./src/parseFiles.ts";
+import { getRecordings } from "./src/parseFiles";
 
 let recs = getRecordings('./toto');
 
-console.log('recs');
+console.log(recs);
