90e41dc89f2cfc81af54fe06f7e0695ffc985ea6
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-07-06 12:26:58 -0400

    Tests: testing `checkIfRecording()`

--- tests/makePage.test.js
+++ tests/makePage.test.js
@@ -1,6 +1,16 @@
-import { getAllScenes } from "../src/parseFiles";
+import parseFiles from "../src/parseFiles";
 
 test("Get all scenes finds enough elements.", () => {
-    let allScenes = getAllScenes("./tests/samples/sample-1");
+    let allScenes = parseFiles.getAllScenes("./tests/samples/sample-1");
     expect((allScenes.length) === 3)
+})
+
+test("Recordings checker on a recording.", () => {
+    let isRecording = parseFiles.checkIfRecording("./tests/samples/samples-1/scene_1/asciicasts/commands_1.cast")
+    expect(isRecording)
+})
+
+test("Recordings checker on a txt file.", () => {
+    let isRecording = parseFiles.checkIfRecording("./tests/samples/sample-1/scene_1/read/read_1.txt")
+    expect(!isRecording)
 })
