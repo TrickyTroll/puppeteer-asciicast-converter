55af77bee33147978f2220629e0164cc9b7101d9
TrickyTroll <53831454+TrickyTroll@users.noreply.github.com>
2021-07-06 12:16:29 -0400

    Fix: proper implementation of `.length`

--- tests/makePage.test.js
+++ tests/makePage.test.js
@@ -2,5 +2,5 @@ import { getAllScenes } from "../src/parseFiles";
 
 test("Get all scenes finds enough elements.", () => {
     let allScenes = getAllScenes("./tests/samples/sample-1");
-    expect(length(allScenes) === 3)
+    expect((allScenes.length) === 3)
 })
